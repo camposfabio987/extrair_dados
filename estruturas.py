6183eff66b2999a29b5ef8cc4c3101352888d090
Fábio Henrique de Sousa Carvalho Campos <fabio.c@inatel.br>
2023-02-08 15:02:26 -0300

    aula 2 feita, básico do python e selenium installed

---
+++ estruturas.py
@@ -0,0 +1,14 @@
+dados = [10,20,30,40,50,60]
+
+print(dados[0])
+print(dados[2])
+
+for elementoAtual in dados:
+    print(elementoAtual)
+
+dados.append(70)
+
+if(10>20):
+    print("Condição verdadeira")
+else:
+    print("Condição falsa")
